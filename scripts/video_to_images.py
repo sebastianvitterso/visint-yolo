7f45b292e42762929173f98630a2752600b1b709
Sebastian <sebastian.vitters@gmail.com>
2021-11-08 11:05:49 +0100

    script

--- scripts/video_to_images.py
+++ scripts/video_to_images.py
@@ -1,16 +1,29 @@
 import cv2
 
-videoname = 'Video00002_ambient'
-video = cv2.VideoCapture(f'./data/train/{videoname}.avi')
-success, frame = video.read()
-print(success)
+input_folder = './data/train/'
+videonames = [
+    'Video00000_ambient',
+    'Video00001_ambient',
+    'Video00002_ambient',
+]
 
-i = 0
-while success:
-    cv2.imwrite(f'./data/train/{videoname}/frame_{i:03}.jpg', frame)
+def input_file(videoname):
+    return f'./data/train/{videoname}.avi'
+
+def output_folder(videoname):
+    return f'./data/train/{videoname}/'
+
+
+for videoname in videonames:
+    video = cv2.VideoCapture(input_file(videoname))
     success, frame = video.read()
-    print(f'Converted frame {i}')
-    i += 1
 
-print(f'Converted {i} frames')
+    i = 0
+    while success:
+        cv2.imwrite(f'{output_folder(videoname)}/frame_{i:03}.jpg', frame)
+        success, frame = video.read()
+        print(f'Converted frame {i}')
+        i += 1
+
+    print(f'Converted {i} frames')
 
